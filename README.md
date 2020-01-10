723c018d6f55c38a92710d19e7d99ac122839154
李家成 <271156993@qq.com>
2020-01-10 09:28:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# mini-images
+
+基于gulp的压缩图片
+
+总共有两种方式  `imagemin`  `tinypng-nokey`
+
+|组件名称	|组件功能	|图片压缩比例	|优势	|劣势|
+|  ----  | ----  | ----  | ----  | ----  |
+|gulp-imagemin	|压缩png、jpg、gif和svg	|各种格式压缩比例大概在10%-20%,有些小图有时不能压缩	|可以压缩的常用格式多，结合imagemin-pngquant 可以提升总体压缩比	|总体压缩比例适中，压缩速度快|
+|gulp-tinypng-nokey	|压缩png，jpg	|jpg压缩比在10%-20%， png50%以上	|png压缩比例高，并且没有限制张数	|不能压缩gif，压缩过程整体速度取决于网速，慢|
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const apiKey = "JKb1RnZyhl6TMfGj4sfSZh2NhphPcLmm";
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const tinyNokey = require('gulp-tinypng-nokey');
+const tiny = require('gulp-tinypng-compress');
+
+gulp.task('tinypng', function(done) {
+    gulp.src('./entry/**/*.{png,jpg,jpeg}')
+        .pipe(tiny({
+            key: apiKey,
+            log: true
+        }))
+        .pipe(gulp.dest('./tinypng'));
+    done()
+});
+
+gulp.task('tinypng-nokey', function(done) {
+    gulp.src('./entry/**/*.{png,jpg,jpeg}')
+        .pipe(tinyNokey())
+        .pipe(gulp.dest('./tinypng-nokey'));
+    done()
+});
+
+// 压缩 public/images 目录内图片(Version>3)
+gulp.task('minify-images', function(done) {
+    gulp.src('./entry/**/*.*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./minify'));
+    done();
+});
+
+gulp.task('default', gulp.series(gulp.parallel('minify-images')), function() {
+    console.log("----------gulp Finished----------");
+    // Do something after a, b, and c are finished.
+});
