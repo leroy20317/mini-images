2d6d6a7d5a276fbede363082019368217875a905
leroy <leroy20317@qq.com>
2020-10-11 10:05:01 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -4,42 +4,42 @@ const imagemin = require('gulp-imagemin');
 const tinyNokey = require('gulp-tinypng-nokey');
 const tiny = require('gulp-tinypng-compress');
 
-gulp.task('tinypng', function(done) {
-    gulp.src('./entry/**/*.{png,jpg,jpeg}')
-        .pipe(tiny({
-            key: apiKey,
-            log: true
-        }))
-        .pipe(gulp.dest('./tinypng'));
-    done()
+gulp.task('tinypng', function (done) {
+  gulp.src('./entry/**/*.{png,jpg,jpeg}')
+    .pipe(tiny({
+      key: apiKey,
+      log: true
+    }))
+    .pipe(gulp.dest('./tinypng'));
+  done()
 });
 
-gulp.task('tinypng-nokey', function(done) {
-    gulp.src('./entry/**/*.{png,jpg,jpeg}')
-        .pipe(tinyNokey())
-        .pipe(gulp.dest('./tinypng-nokey'));
-    done()
+gulp.task('tinypng-nokey', function (done) {
+  gulp.src('./entry/**/*.{png,jpg,jpeg}')
+    .pipe(tinyNokey())
+    .pipe(gulp.dest('./tinypng-nokey'));
+  done()
 });
 
 // 压缩 public/images 目录内图片(Version>3)
-gulp.task('minify-images', function(done) {
-    gulp.src('./entry/**/*.*')
-        .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.jpegtran({progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
-        .pipe(gulp.dest('./minify'));
-    done();
+gulp.task('minify-images', function (done) {
+  gulp.src('./entry/**/*.*')
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ]))
+    .pipe(gulp.dest('./minify'));
+  done();
 });
 
-gulp.task('default', gulp.series(gulp.parallel('minify-images')), function() {
-    console.log("----------gulp Finished----------");
-    // Do something after a, b, and c are finished.
+gulp.task('default', gulp.series(gulp.parallel('minify-images')), function () {
+  console.log("----------gulp Finished----------");
+  // Do something after a, b, and c are finished.
 });
